41ec755663cd992f9493a75f18c85bfbd7f587a5
Kjetil <80390887+Cheteel@users.noreply.github.com>
2023-02-02 15:27:57 +0000

    new file:   .browserslistrc 	new file:   .eslintrc.js 	modified:   .gitignore 	new file:   README.md 	new file:   babel.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   src/App.vue 	new file:   src/assets/logo.png 	new file:   src/assets/logo.svg 	new file:   src/main.ts 	new file:   src/plugins/vuetify.js 	new file:   src/plugins/vuetify.ts 	new file:   src/router/index.ts 	new file:   src/shims-tsx.d.ts 	new file:   src/shims-vue.d.ts 	new file:   src/shims-vuetify.d.ts 	new file:   src/store/index.ts 	new file:   src/views/HomeView.vue 	new file:   tsconfig.json 	new file:   vue.config.js

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <h1>Cheteel</h1>
+    </v-app-bar>
+    <v-main></v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App',
+
+  data: () => ({
+    //
+  })
+})
+</script>
